041abc5ce34988febb397e56a7e4bdcfab041e0c
krishnagupta1543 <kg18960@gmail.com>
2025-03-28 04:28:07 +0530

    process.env.PORT added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use('/api/v1', rootRouter);
 
 
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
